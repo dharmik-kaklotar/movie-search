b5703c874442e68308274963a2802052ca16cacd
Dharmik Kaklotar <161474206+dharmik-kaklotar@users.noreply.github.com>
2024-03-25 15:46:46 +0530

    Update MovieList.jsx

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -12,7 +12,7 @@ const Home = () => {
 
   // for get your api  key here: https://www.omdbapi.com/apikey.aspx
 
-  const apiKey = '289b9834';
+  const apiKey = 'your Api Key';
 
   const GetMovies = async (movieName) => {
     setLoding(true);
